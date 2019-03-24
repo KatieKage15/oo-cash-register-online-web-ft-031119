d0313bb9ac860fcd79f3e420c8c7cb9d0b80d0d4
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-24 19:50:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,3 +7,13 @@ class CashRegister
     @discount = discount
     @items =[]
   end
+
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
+
+  
